303e2c174b655acbad764092b358aafd94078b18
Diamond_TECH <ezehemmanuel200118@gmail.com>
2025-11-07 09:48:02 +0000

    h

--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -34,7 +34,8 @@ export const validateRegister = [
     if (!errors.isEmpty()) {
       return res.status(400).json({
         success: false,
-        errors: errors.array().map(err => err.msg),
+        errors: errors.array()[0].msg
+        // errors: errors.array().map(err => err.msg),
       });
     }
     next();
