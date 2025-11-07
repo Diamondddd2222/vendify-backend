b7182707219e900e5edef6ca1dcf03be0eb44bdd
Diamond_TECH <ezehemmanuel200118@gmail.com>
2025-11-07 06:23:22 +0000

    validation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/userControllers.js";
+import { validateRegister, validateLogin } from "../middlewares/validationMiddleware.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/register", validateRegister,  registerUser);
+router.post("/login", validateLogin, loginUser);
 
 export default router;
