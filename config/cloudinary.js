4bf0460e3da58bc5cacbc9689afc9abde7e93d67
Diamond_TECH <ezehemmanuel200118@gmail.com>
2025-11-09 12:39:37 +0000

    y

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -9,13 +9,13 @@ const config = {
   api_secret: process.env.CLOUDINARY_API_SECRET,
 };
 
-if (process.env.DEBUG) {
-  console.log('Cloudinary Config:', {
-    cloud_name: config.cloud_name ? 'Set' : 'Missing',
-    api_key: config.api_key ? 'Set' : 'Missing',
-    api_secret: config.api_secret ? 'Set' : 'Missing'
-  });
-}
+// if (process.env.DEBUG) {
+//   console.log('Cloudinary Config:', {
+//     cloud_name: config.cloud_name ? 'Set' : 'Missing',
+//     api_key: config.api_key ? 'Set' : 'Missing',
+//     api_secret: config.api_secret ? 'Set' : 'Missing'
+//   });
+// }
 
 cloudinary.config(config);
 
